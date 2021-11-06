68001619729e6bb55ea9e76de322e2ed234d7d6c
Yap Zi Yann <32502408+KelvinYapZiYann@users.noreply.github.com>
2021-11-06 20:37:09 +0800

    import router

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from "vue-router";
+import router from "./router/index";
 import i18n from "./i18n";
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+
 new Vue({
+  router,
   i18n,
   render: h => h(App),
 }).$mount('#app')
