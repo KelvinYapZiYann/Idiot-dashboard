89f48702bfe6b1ec81e6768bba08f1c7be42eff7
Yap Zi Yann <32502408+KelvinYapZiYann@users.noreply.github.com>
2021-11-19 22:12:12 +0800

    in store traffic detail

---
+++ src/pages/Resources/InStoreTraffic/InStoreTrafficDetail.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="content col-xl-10 col-lg-12 col-md-12 ml-auto mr-auto">
+    <base-detail
+     :title="$t('component.store')"
+     :model="resource.model"
+     :headers="detail.detailHeaders"
+     thead-classes="text-primary"
+    ></base-detail>
+
+    <div class="row">
+        <div class="col-lg-6 col-md-6 col-sm-6 col-6">
+            <stats-card
+                :title="resource.model.enter"
+                :sub-title="$t('property.enter')"
+                type="primary"
+                icon="fas fa-sign-in-alt"
+                >
+            </stats-card>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-6">
+            <stats-card
+                :title="resource.model.exit"
+                :sub-title="$t('property.exit')"
+                type="warning"
+                icon="fas fa-sign-out-alt"
+                >
+            </stats-card>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-6">
+            <stats-card
+                :title="resource.model.passing"
+                :sub-title="$t('property.passing')"
+                type="neutral"
+                icon="fas fa-times"
+                >
+            </stats-card>
+        </div>
+        <div class="col-lg-6 col-md-6 col-sm-6 col-6">
+            <stats-card
+                :title="resource.model.return"
+                :sub-title="$t('property.return')"
+                type="success"
+                icon="fas fa-undo-alt"
+                >
+            </stats-card>
+        </div>
+    </div>
+
+    <base-button slot="footer" type="primary" @click="handleBack()" fill>
+      <i class="fas fa-chevron-left mr-1"></i>
+      {{$t('component.back')}}
+    </base-button>
+    <base-button slot="footer" type="primary" @click="handleEdit()" fill>
+      <i class="fas fa-edit mr-1"></i>
+      {{$t('component.edit')}} {{$t('component.inStoreTraffic')}}
+    </base-button>
+  </div>
+</template>
+<script>
+import { BaseButton, BaseDetail, StatsCard } from "@/components";
+import router from "@/router";
+
+export default {
+  components: {
+    BaseButton,
+    BaseDetail,
+    StatsCard
+  },
+  data() {
+    return {
+      inStoreTrafficId: this.$route.params.inStoreTrafficId,
+      resource: {
+        model: {
+            enter: '0',
+            exit: '0',
+            passing: '0',
+            return: '0',
+        },
+        data: {}
+      },
+      detail: {
+        detailHeaders: {
+          name: this.$t('property.name'),
+          macAddress: this.$t('property.macAddress'),
+          status: this.$t('property.status'),
+        },
+      }
+    };
+  },
+  props: {
+    previousRoute: {
+      type: String,
+      required: false,
+      default: "",
+      description: "Previous Route"
+    }
+  },
+  mounted() {
+    this.getResource();
+  },
+  methods: {
+    async getResource() {
+      let loader = this.$loading.show();
+      try {
+        await this.$store.dispatch('inStoreTraffic/getById', this.storeId).then(() => {
+          this.resource.model = this.$store.getters["store/models"][0];
+          this.resource.data = this.$store.getters["asset/data"];
+        });
+      } catch (e) {
+        console.error(e);
+      } finally {
+        loader.hide();
+      }
+    },
+    async handleBack() {
+      if (this.previousRoute) {
+        router.push({
+          name: this.previousRoute, 
+          params: {
+            previousRouteParam: this.previousRouteParam
+          }
+        });
+      } else {
+        router.go(-1);
+      }
+    },
+    async handleEdit() {
+      router.push({
+        name: "Edit In Store Traffic",
+        params: {
+          inStoreTrafficId: this.inStoreTrafficId,
+          previousRoute: router.currentRoute.name,
+        }
+      });
+    },
+  }
+};
+</script>
+<style>
+</style>
