13f17c6b25c76e7bfd38d37371ca4218062b7a77
Yap Zi Yann <32502408+KelvinYapZiYann@users.noreply.github.com>
2021-11-25 23:00:05 +0800

    toggle sidebar fixed

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,16 @@
 
 export default {
   name: 'App',
-  components: {}
+  components: {},
+  methods: {
+    toggleNavOpen() {
+      let root = document.getElementsByTagName("html")[0];
+      root.classList.toggle("nav-open");
+    },
+  },
+  mounted() {
+    this.$watch("$sidebar.showSidebar", this.toggleNavOpen);
+  },
 }
 </script>
 
